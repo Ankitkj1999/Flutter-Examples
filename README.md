e36320b40fc28fb4539bc922f6793bb55f046770
Ankit Kumar <57802410+Ankitkj1999@users.noreply.github.com>
2022-12-31 01:31:52 +0530

    app in working state

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
-# Flutter Firebase Example
+# Flutter Flash Light Example App
 
-This is an application which displays a list of grocery items or strings stored and retrieved from firebase firestore. This app show the application of CROD operations.
+### In this flutter app I have made a flash light example app using the following flutter packages:
+<ul>
+<li><i>torch_light</i></li>
+<li><i>day_night_switcher</i></li>
+<li><i>url_launcher</i></li>
+<li><i>flutter_native_splach</i></li>
 
+</ul>
+
+> ### Learnings from this app would be :
+> 1. Implemtaion of conditioal statement in many places as we have changed the color and image in many places by checking the when the dark mode if on or not.
+> 2. Implemneted native splash screen.
+> 3. Used the flshlight feature.
 
 ## Screenshots
 
-| App | Working | 
-| ---------------- | ---------------- | 
-| ![](https://raw.githubusercontent.com/Ankitkj1999/flutter_firestore_example/master/screen_one.gif) | ![](https://raw.githubusercontent.com/Ankitkj1999/flutter_firestore_example/master/screen_two.gif)|
-
-| Firestore | 
-| ------------ | 
-| ![](https://raw.githubusercontent.com/Ankitkj1999/flutter_firestore_example/master/screen_three.png) | 
+| App | Working |
+| ---------------- | ---------------- |
+|![](output.gif) | ![](output_2.gif)|
--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:day_night_switcher/day_night_switcher.dart';
+import 'package:torch_light/torch_light.dart';
+import '../widgets.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Flash Light App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "Oxygen",
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Main(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
+class Main extends StatefulWidget {
+  const Main({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Main> createState() => _MainState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _MainState extends State<Main> {
+  bool isFlashOn = true;
+  bool isDarkMode = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final w = MediaQuery.of(context).size.width;
+    final h = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: isDarkMode
+            ? const Color.fromARGB(255, 245, 245, 245)
+            : const Color.fromARGB(255, 24, 24, 24),
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          title: Text(
+            "Flashy",
+            style: TextStyle(
+                color: isDarkMode ? Colors.black : Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.w600),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                child: Container(
+                  width: w / 8.5,
+                  height: h / 17.5,
+                  child: isDarkMode
+                      ? Image(image: AssetImage("assets/images/light_on.png"))
+                      : Image(image: AssetImage("assets/images/light_off.png")),
+                ),
+                bottom: h / 1.5,
+              ),
+              Positioned(
+                child: DayNightSwitcher(
+                  starsColor: Colors.transparent,
+                  cloudsColor: Colors.transparent,
+                  isDarkModeEnabled: isFlashOn,
+                  onStateChanged: (val) {
+                    setState(() {
+                      isFlashOn = val;
+                      isDarkMode = !isDarkMode;
+                    });
+                    isFlashOn ? _turnOffFlash(context) : _turnOnFlash(context);
+                  },
+                ),
+                bottom: h / 2.2,
+              ),
+              Positioned(
+                bottom: h / 7.5,
+                child: Text(
+                  "Made By Ankit Kumar",
+                  style: TextStyle(
+                      color: isDarkMode ? Colors.black : Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+              Positioned(
+                  bottom: 20,
+                  child: SizedBox(
+                    width: w / 1.2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        bottomSocial(
+                            img: 'assets/images/gmail.png',
+                            url: "https://ankit.k.j1999@gmail.com",
+                            context: context,
+                            isDarkMode: isDarkMode),
+                        bottomSocial(
+                            img: 'assets/images/twitter.png',
+                            url: "https://twitter.com/ankitkj1999",
+                            context: context,
+                            isDarkMode: isDarkMode),
+                        bottomSocial(
+                            img: 'assets/images/linkedin.png',
+                            url: "https://www.linkedin.com/in/ankitkj1999/",
+                            context: context,
+                            isDarkMode: isDarkMode),
+                        bottomSocial(
+                            img: 'assets/images/github.png',
+                            url: "https://github.com/ankitkj1999",
+                            context: context,
+                            isDarkMode: isDarkMode),
+                      ],
+                    ),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+Future<void> _turnOnFlash(BuildContext context) async {
+  try {
+    await TorchLight.enableTorch();
+  } on Exception catch (_) {
+    _showErrorMessage("Could not enable Flashlight", context);
+  }
+}
+
+Future<void> _turnOffFlash(BuildContext context) async {
+  try {
+    await TorchLight.disableTorch();
+  } on Exception catch (_) {
+    _showErrorMessage("Could not enable Flashlight", context);
+  }
+}
+
+void _showErrorMessage(String mes, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mes)));
 }
